6a5fdbbfbf812a7bf746309569b187909d3950da
Gabriel <gabriel.gsm2017@gmail.com>
2022-03-02 16:48:52 -0300

    Criando projeto React

---
+++ secao-4/react-basico/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Olá mundo, meu primeiro projeto em React</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
