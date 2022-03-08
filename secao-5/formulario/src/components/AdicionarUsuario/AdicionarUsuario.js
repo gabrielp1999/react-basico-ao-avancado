3ffbef0c77825ed2d7fee1edf4813776c2314ab5
Gabriel <gabriel.gsm2017@gmail.com>
2022-03-08 17:29:51 -0300

    Método POST

--- secao-5/formulario/src/components/AdicionarUsuario/AdicionarUsuario.js
+++ secao-5/formulario/src/components/AdicionarUsuario/AdicionarUsuario.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react'
 
 import './AdicionarUsuario.css'
 
+const INITTIAL_STATE = { 
+  usuario: { nome: '', sobrenome: '', email: '' } 
+}
+
 class AdicionarUsuario extends Component {
 
   constructor(props) {
     super(props)
 
-    this.state = { 
-      usuario: { nome: '', sobrenome: '', email: '' } 
-    }
+
+
+    this.state =INITTIAL_STATE
 
     this.onChangeHandler = this.onChangeHandler.bind(this)
     this.onSubmitHandler = this.onSubmitHandler.bind(this)
@@ -22,11 +26,20 @@ class AdicionarUsuario extends Component {
 
   onSubmitHandler(event) {
     event.preventDefault()
-    const id = Math.floor(Math.random() * 1000)
-    const usuario = { ...this.state.usuario, id }
+    const usuario = this.state.usuario
 
-    this.setState({ usuario: { nome: '', sobrenome: '', email: '' } })
-    this.props.adicionarUsuario(usuario)
+    fetch('https://reqres.in/api/users',{
+      method: "POST",
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify(usuario)
+     })
+    .then(resp => resp.json())
+    .then(dados => {
+      console.log(dados)
+      this.setState(INITTIAL_STATE)
+      this.props.adicionarUsuario(dados)
+    })
+
   }
 
   render() {
