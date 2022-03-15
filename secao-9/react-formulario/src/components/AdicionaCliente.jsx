abf6ffad272c9c361c5aec3f333d29e96cb6dbb7
Gabriel <gabriel.gsm2017@gmail.com>
2022-03-15 20:38:24 -0300

    Formik

---
+++ secao-9/react-formulario/src/components/AdicionaCliente.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Formik } from 'formik';
+
+const AdicionaCliente = () => {
+  return (
+    <>
+      <h1>Cadastro de Clientes</h1>
+
+    <Formik initialValues={{ nome:'', email:'',nascimento:'' }}
+      onSubmit={(values) => {
+        alert(JSON.stringify(values));
+      }}
+    >
+      {(props) => (
+      <form onSubmit={props.handleSubmit} noValidate  >
+        <div className="form-group">
+          <label htmlFor="nome">Nome</label>
+          <input id="nome"
+           name="nome"
+            type="text" 
+            onChange={props.handleChange}
+            value={props.values.nome}/>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input id="email"
+           name="email"
+          type="email"
+           onChange={props.handleChange}
+           value={props.values.email} />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="date">Data de Nascimento</label>
+          <input id="nascimento"
+           name="nascimento"
+          type="date" 
+          onChange={props.handleChange}
+          value={props.values.nascimento}
+          />
+        </div>
+
+        <button type="submit">Adicionar</button>
+      </form>
+      )}
+    </Formik>
+    </>
+  );
+};
+
+export default AdicionaCliente;
